beb11066dd5231f9dfffae01830dd58aff644fae
beaar <beaar@users.noreply.github.com>
2016-11-30 18:06:33 +0100

    Separate USM backpack fix from mag compat

--- addons/tm_mag_compat/config.cpp
+++ addons/tm_mag_compat/config.cpp
@@ -14,8 +14,7 @@ class CfgPatches
 			"A3_Weapons_F",
 			"hlcweapons_core","hlcweapons_aks",
 			"rhs_c_weapons","rhsusf_c_weapons",
-			"cup_weapons_grenadelaunchers","cup_weapons_l85","cup_weapons_m249","cup_weapons_mk48","cup_weapons_scar","cup_weapons_xm8",
-			"us_military_units"
+			"cup_weapons_grenadelaunchers","cup_weapons_l85","cup_weapons_m249","cup_weapons_mk48","cup_weapons_scar","cup_weapons_xm8"
 		};
 		requiredVersion = REQUIRED_VERSION;
 		VERSION_CONFIG;
@@ -23,5 +22,4 @@ class CfgPatches
 };
 
 #include "CfgMagazines.hpp"
-#include "CfgVehicles.hpp"
 #include "CfgWeapons.hpp"
