471bbbf85c2fc2657dee1747ca596fadb18a0956
Bear <beaar@users.noreply.github.com>
2016-12-02 01:56:59 +0100

    Increase mass for 60rnd STANAG (#4)

--- addons/tm_mag_compat/CfgMagazines.hpp
+++ addons/tm_mag_compat/CfgMagazines.hpp
@@ -6,6 +6,7 @@ class CfgMagazines
 		descriptionshort = "Caliber: 5.56x45mm NATO<br />Rounds: 60<br />Used in: M27 IAR";
 		displayname = "5.56mm M855A1 60rnd Mag";
 		count = 60;
+		mass = 14;
 	};
 	class rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red: rhs_mag_30Rnd_556x45_M855A1_Stanag{};
 	class delta_rhs_mag_60Rnd_556x45_M855A1_Stanag_Tracer_Red: rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red
@@ -13,5 +14,6 @@ class CfgMagazines
 		descriptionshort = "Caliber: 5.56x45mm NATO<br />Rounds: 60<br />Used in: M27 IAR";
 		displayname = "5.56mm M855A1 Tracer 60rnd Mag";
 		count = 60;
+		mass = 14;
 	};
 };
