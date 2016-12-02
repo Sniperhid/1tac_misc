7012770449a23b8229ced7b718e5f2c1e203db13
beaar <beaar@users.noreply.github.com>
2016-12-02 11:39:17 +0100

    Hide Flashbangs
    Obsoletes bear_hide_flashbang

---
+++ addons/tm_hide_flashbangs/CfgMagazines.hpp
@@ -0,0 +1,35 @@
+class CfgMagazines {
+	class rhs_VOG25;
+	class rhs_VG40SZ: rhs_VOG25 {
+		HIDE_THIS
+	};
+	
+	class HandGrenade;
+	class rhs_mag_fakel: HandGrenade {
+		HIDE_THIS
+	};
+	class rhs_mag_fakels: HandGrenade {
+		HIDE_THIS
+	};
+	class rhs_mag_zarya2: rhs_mag_fakels {
+		HIDE_THIS
+	};
+	class rhs_mag_plamyam: rhs_mag_fakels {
+		HIDE_THIS
+	};
+	
+	class rhs_mag_mk84: HandGrenade {
+		HIDE_THIS
+	};
+	class rhs_mag_m7a3_cs: HandGrenade {
+		HIDE_THIS
+	};
+	
+	class rhs_mag_mk3a2: HandGrenade {
+		HIDE_THIS
+	};
+	
+	class ACE_M84: HandGrenade {
+		HIDE_THIS
+	};
+};
