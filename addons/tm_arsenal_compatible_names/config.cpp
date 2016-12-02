4037c9bb680e732ceb39e8056e68f48458f6fe08
Bear <beaar@users.noreply.github.com>
2016-12-02 12:16:45 +0100

    Arsenal compatible names (#10)
    Obsoletes arsenal_compatible_names

---
+++ addons/tm_arsenal_compatible_names/config.cpp
@@ -0,0 +1,21 @@
+#include "script_component.hpp"
+
+class CfgPatches
+{
+	class ADDON
+	{
+        name = "Arsenal compatible names";
+		author = "Bear";
+		url = "http://www.teamonetactical.com";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {
+			"rhs_c_weapons",
+			"hlcweapons_aks"
+		};
+		requiredVersion = REQUIRED_VERSION;
+		VERSION_CONFIG;
+	};
+};
+
+#include "CfgWeapons.hpp"
