76c26fe63a14f8285c1f72e22e34889b6eec59f2
beaar <beaar@users.noreply.github.com>
2016-12-02 11:28:37 +0100

    Hide fluff HLC AUG variants
    Obsoletes bear_hide_hlc_aug

---
+++ addons/tm_hide_hlg_aug/config.cpp
@@ -0,0 +1,23 @@
+#include "script_component.hpp"
+
+class CfgPatches
+{
+	class ADDON
+	{
+        name = "Hide fluff AUG variants";
+		author = "Bear";
+		url = "http://www.teamonetactical.com";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {
+			"hlcweapons_AUG"
+		};
+		requiredVersion = REQUIRED_VERSION;
+		VERSION_CONFIG;
+	};
+};
+
+#define HIDE_THIS scope = 1; scopeArsenal = 1; scopeCurator = 1; 
+#define SHOW_THIS scope = 2; scopeArsenal = 2; scopeCurator = 2; 
+
+#include "CfgWeapons.hpp"
