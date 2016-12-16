a3f313e609cb8af14dbbacd20917303a36bba95e
beaar <beaar@users.noreply.github.com>
2016-12-16 11:17:42 +0100

    Hide misc
    hush hush

---
+++ addons/tm_hide_misc/CfgWeapons.hpp
@@ -0,0 +1,24 @@
+class CfgWeapons
+{
+	class Itemcore;
+	class xmas_santa_uniform: Itemcore
+	{
+		scope = 1;
+		scopeArsenal = 0;
+	};
+	class xmas_santa_blufor_uniform: xmas_santa_uniform
+	{
+		scope = 1;
+		scopeArsenal = 0;
+	};
+	class xmas_santa_opfor_uniform: xmas_santa_uniform
+	{
+		scope = 1;
+		scopeArsenal = 0;
+	};
+	class xmas_santa_ind_uniform: xmas_santa_uniform
+	{
+		scope = 1;
+		scopeArsenal = 0;
+	};
+};
