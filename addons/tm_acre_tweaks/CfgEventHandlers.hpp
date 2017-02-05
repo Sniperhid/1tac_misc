c0d70c3f9304d418a8ed73b580b864011dcb0a6e
Sniperhid <Sniperhid@users.noreply.github.com>
2017-02-05 23:27:57 +0000

    Add a custom ACRE2 signal function

---
+++ addons/tm_acre_tweaks/CfgEventHandlers.hpp
@@ -0,0 +1,5 @@
+class Extended_PostInit_EventHandlers {
+    class acre_signal_fix {
+        init = "call compile preProcessFileLineNumbers 'x\tac1\addons\tm_acre_fix\XEH_postInit.sqf'";
+    };
+};
---
+++ addons/tm_acre_tweaks/config.cpp
@@ -0,0 +1,16 @@
+
+class CfgPatches
+{
+    class tm_acre_signal_fix
+    {
+        name = "ACRE SQF Signal Calc Override";
+        author = "Snippers";
+        url = "http://www.teamonetactical.com";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"cba_main","acre_main"};
+    };
+};
+
+#include "CfgEventHandlers.hpp"
