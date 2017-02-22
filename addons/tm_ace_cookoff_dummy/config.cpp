94ca8ef4b70c840922580ce2917e2acf75a84aab
beaar <beaar@users.noreply.github.com>
2017-02-22 13:55:14 +0100

    ACE Cookoff Dummy

---
+++ addons/tm_ace_cookoff_dummy/config.cpp
@@ -0,0 +1,28 @@
+#include "script_component.hpp"
+
+class CfgPatches
+{
+	class ADDON
+	{
+		name = "ACE Cookoff Dummy Addon";
+		author = "Bear";
+		url = "http://www.teamonetactical.com";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		VERSION_CONFIG;
+	};
+	
+	class ace_cookoff
+	{
+		name = "1TAC_MISC: ACE Cookoff Dummy Addon";
+		author = "Bear";
+		url = "http://www.teamonetactical.com";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		VERSION_CONFIG;
+	};
+};
---
+++ addons/tm_ace_cookoff_dummy/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT tm_ace_cookoff_dummy
+
+#include "\x\tac1\addons\tm_main\script_mod.hpp"
+#include "\x\tac1\addons\tm_main\script_macros.hpp"
+
