665f8305a7768b8c624f67476504d67d20d552bd
Bear <beaar@users.noreply.github.com>
2017-04-16 15:49:51 +0200

    NIArms MG3 sounds for JO_MG3 (#59)

---
+++ addons/tm_mg3_niarms_sounds/README.md
@@ -0,0 +1,3 @@
+These sounds are from NIArms MG3 pack (hlc_wp_mg3): https://github.com/toadie2k/NIArms
+
+The NIArms release thread ( https://forums.bistudio.com/forums/topic/190455-niarms-release-thread/ ) states it's licensed under APL-SA (with additional restrictions), so the same applies here.
---
+++ addons/tm_mg3_niarms_sounds/niarms/cfgSoundshaders.hpp
@@ -0,0 +1,185 @@
+﻿
+class CfgSoundShaders
+{
+	class MG3_Closure_SoundShader
+	{
+		samples[] = {
+			{ "x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_first", 1 } };
+		range = 5;
+        volume = 0.8;
+	};
+	class MG3_closeShot_SoundShader
+	{
+		samples[] = {
+			{ "x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_close1", 1 },
+			{ "x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_close2", 1 },
+			{ "x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_close3", 1 } };
+        volume = 0.8;
+		range = 50;
+		rangeCurve = "closeShotCurve";
+	};
+	class MG3_midShot_SoundShader
+	{
+		samples[] = {
+			{ "x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_mid1", 1 },
+			{ "x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_mid2", 1 },
+			{ "x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_mid3", 1 } };
+		volume = 0.5011872;
+		range = 2000;
+		rangeCurve[] = {
+			{ 0, 0.2 },
+			{ 50, 1 },
+			{ 300, 0 },
+			{ 2000, 0 } };
+	};
+	class MG3_distShot_SoundShader
+	{
+		samples[] = {
+			{ "x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_dist1", 1 },
+			{ "x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_dist2", 1 },
+			{ "x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_dist3", 1 } };
+		volume = 1.0;
+		range = 2000;
+		rangeCurve[] = {
+			{ 0, 0 },
+			{ 50, 0 },
+			{ 300, 1 },
+			{ 2000, 1 } };
+	};
+	class MG3_tailDistant_SoundShader
+	{
+		samples[] = {
+			{ "\A3\Sounds_F\arsenal\weapons\Machineguns\Zafir\Zafir_taildistant", 1 } };
+		volume = 1.0;
+		range = 2000;
+		rangeCurve[] = {
+			{ 0, 0 },
+			{ 600, 0.7 },
+			{ 2000, 1 } };
+		limitation = 1;
+	};
+	class MG3_tailForest_SoundShader
+	{
+		samples[] = {
+			{ "\A3\Sounds_F\arsenal\weapons\Machineguns\Zafir\Zafir_tailforest", 1 } };
+		volume = "(1-interior/1.4)*forest/3";
+		range = 2000;
+		rangeCurve[] = {
+			{ 0, 1 },
+			{ 2000, 0 } };
+		limitation = 1;
+	};
+	class MG3_tailHouses_SoundShader
+	{
+		samples[] = {
+			{ "\A3\Sounds_F\arsenal\weapons\Machineguns\Zafir\Zafir_tailhouses", 1 } };
+		volume = "(1-interior/1.4)*houses/3";
+		range = 1500;
+		rangeCurve[] = {
+			{ 0, 1 },
+			{ 250, 0.3 },
+			{ 1500, 0 } };
+		limitation = 1;
+	};
+	class MG3_tailInterior_SoundShader
+	{
+		samples[] = {
+			{ "x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_indoor", 1 } };
+		volume = "interior";
+		range = 350;
+		rangeCurve[] = {
+			{ 0, 1 },
+			{ 50, 0.4 },
+			{ 100, 0.2 },
+			{ 350, 0 } };
+		limitation = 1;
+	};
+	class MG3_tailMeadows_SoundShader
+	{
+		samples[] = {
+			{ "\x\tac1\addons\tm_mg3_niarms_sounds\niarms\snd\soundshaders\MG3\MG3_meadow", 1 } };
+		volume = "(1-interior/1.4)*(meadows/2 max sea/2)/3";
+		range = 2000;
+		rangeCurve[] = {
+			{ 0, 1 },
+			{ 2000, 0 } };
+		limitation = 1;
+	};
+	class MG3_tailTrees_SoundShader
+	{
+		samples[] = {
+			{ "\A3\Sounds_F\arsenal\weapons\Machineguns\Zafir\Zafir_tailtrees", 1 } };
+		volume = "(1-interior/1.4)*trees/3";
+		range = 2000;
+		rangeCurve[] = {
+			{ 0, 1 },
+			{ 2000, 0 } };
+		limitation = 1;
+	};
+	class MG3_silencerShot_SoundShader
+	{
+		samples[] = {
+			{ "A3\Sounds_F_Mark\arsenal\weapons\Machineguns\MMG_01_Navid\MMG01_silencerShot_01", 1 },
+			{ "A3\Sounds_F_Mark\arsenal\weapons\Machineguns\MMG_01_Navid\MMG01_silencerShot_02", 1 },
+			{ "A3\Sounds_F_Mark\arsenal\weapons\Machineguns\MMG_01_Navid\MMG01_silencerShot_03", 1 } };
+		volume = 1.0;
+		range = 150;
+		rangeCurve = "closeShotCurve";
+	};
+	class MG3_silencerTailForest_SoundShader
+	{
+		samples[] = {
+			{ "A3\Sounds_F_Mark\arsenal\weapons\Machineguns\MMG_01_Navid\MMG01_silencerTailForest", 1 } };
+		volume = "(1-interior/1.4)*forest/3";
+		range = 150;
+		rangeCurve[] = {
+			{ 0, 1 },
+			{ 150, 0.3 } };
+		limitation = 1;
+	};
+	class MG3_silencerTailHouses_SoundShader
+	{
+		samples[] = {
+			{ "A3\Sounds_F_Mark\arsenal\weapons\Machineguns\MMG_01_Navid\MMG01_silencerTailHouses", 1 } };
+		volume = "(1-interior/1.4)*houses/3";
+		range = 150;
+		rangeCurve[] = {
+			{ 0, 1 },
+			{ 150, 0 } };
+		limitation = 1;
+	};
+	class MG3_silencerTailInterior_SoundShader
+	{
+		samples[] = {
+			{ "A3\Sounds_F_Mark\arsenal\weapons\Machineguns\MMG_01_Navid\MMG01_silencerTailInterior", 1 } };
+		volume = "interior";
+		range = 150;
+		rangeCurve[] = {
+			{ 0, 1 },
+			{ 50, 0.3 },
+			{ 150, 0 } };
+		limitation = 1;
+	};
+	class MG3_silencerTailMeadows_SoundShader
+	{
+		samples[] = {
+			{ "A3\Sounds_F_Mark\arsenal\weapons\Machineguns\MMG_01_Navid\MMG01_silencerTailMeadows", 1 } };
+		volume = "(1-interior/1.4)*(meadows/2 max sea/2)/3";
+		range = 150;
+		rangeCurve[] = {
+			{ 0, 1 },
+			{ 150, 0.3 } };
+		limitation = 1;
+	};
+	class MG3_silencerTailTrees_SoundShader
+	{
+		samples[] = {
+			{ "A3\Sounds_F_Mark\arsenal\weapons\Machineguns\MMG_01_Navid\MMG01_silencerTailTrees", 1 } };
+		volume = "(1-interior/1.4)*trees/3";
+		range = 150;
+		rangeCurve[] = {
+			{ 0, 1 },
+			{ 150, 0.3 } };
+		limitation = 1;
+	};
+};
