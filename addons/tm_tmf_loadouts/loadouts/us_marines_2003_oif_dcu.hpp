c6ca7b7383573c739f8451f2a905f795a9d53033
Bear <nipronz@gmail.com>
2017-06-18 20:14:04 +0200

    💩

--- addons/tm_tmf_loadouts/loadouts/us_marines_2003_oif_dcu.hpp
+++ addons/tm_tmf_loadouts/loadouts/us_marines_2003_oif_dcu.hpp
@@ -335,7 +335,7 @@ class hatg : car
 	backpack[] = {"B_Carryall_khk"};
 	backpackItems[] =
 	{
-		LIST_2("rhs_fgm148_magazine_AT")
+		"rhs_fgm148_magazine_AT"
 	};
 };
 class hatac : r
@@ -344,7 +344,7 @@ class hatac : r
 	backpack[] = {"B_Carryall_khk"};
 	backpackItems[] =
 	{
-		LIST_2("rhs_fgm148_magazine_AT")
+		"rhs_fgm148_magazine_AT"
 	};
 };
 class hatag : car
@@ -353,7 +353,7 @@ class hatag : car
 	backpack[] = {"B_Carryall_khk"};
 	backpackItems[] =
 	{
-		LIST_2("rhs_fgm148_magazine_AT")
+		"rhs_fgm148_magazine_AT"
 	};
 	linkedItems[] =
 	{
