f68bf04e64140fc9e3d8a9d80425f951dbe1d766
Bear <beaar@users.noreply.github.com>
2017-06-18 23:50:52 +0200

    Hide CUP SCAR fluff 👺 (#69)

--- addons/tm_hide_cup_duplicate_weapons/CfgWeapons.hpp
+++ addons/tm_hide_cup_duplicate_weapons/CfgWeapons.hpp
@@ -2,70 +2,125 @@ class CfgWeapons
 {
 	class Rifle_Base_F;
 	class CUP_arifle_AK_Base: Rifle_Base_F {};
-	class CUP_arifle_AK74: CUP_arifle_AK_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AK74_GL: CUP_arifle_AK_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AK74M: CUP_arifle_AK_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AK74M_GL: CUP_arifle_AK_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AK47: CUP_arifle_AK_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AKM: CUP_arifle_AK_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AKS: CUP_arifle_AKM { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AKS_Gold: CUP_arifle_AKS { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AKS_Base: CUP_arifle_AK_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AKS74: CUP_arifle_AKS_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; }; //?!?!? wot
-	class CUP_arifle_AKS74U: CUP_arifle_AKS_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AK107_Base: CUP_arifle_AK_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AK107: CUP_arifle_AK107_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_AK107_GL: CUP_arifle_AK107_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_RPK74: CUP_arifle_AK74 { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_RPK74_45: CUP_arifle_RPK74 { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_RPK74M: CUP_arifle_RPK74_45 { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_arifle_AK74: CUP_arifle_AK_Base { HIDE };
+	class CUP_arifle_AK74_GL: CUP_arifle_AK_Base { HIDE };
+	class CUP_arifle_AK74M: CUP_arifle_AK_Base { HIDE };
+	class CUP_arifle_AK74M_GL: CUP_arifle_AK_Base { HIDE };
+	class CUP_arifle_AK47: CUP_arifle_AK_Base { HIDE };
+	class CUP_arifle_AKM: CUP_arifle_AK_Base { HIDE };
+	class CUP_arifle_AKS: CUP_arifle_AKM { HIDE };
+	class CUP_arifle_AKS_Gold: CUP_arifle_AKS { HIDE };
+	class CUP_arifle_AKS_Base: CUP_arifle_AK_Base { HIDE };
+	class CUP_arifle_AKS74: CUP_arifle_AKS_Base { HIDE }; //?!?!? wot
+	class CUP_arifle_AKS74U: CUP_arifle_AKS_Base { HIDE };
+	class CUP_arifle_AK107_Base: CUP_arifle_AK_Base { HIDE };
+	class CUP_arifle_AK107: CUP_arifle_AK107_Base { HIDE };
+	class CUP_arifle_AK107_GL: CUP_arifle_AK107_Base { HIDE };
+	class CUP_arifle_RPK74: CUP_arifle_AK74 { HIDE };
+	class CUP_arifle_RPK74_45: CUP_arifle_RPK74 { HIDE };
+	class CUP_arifle_RPK74M: CUP_arifle_RPK74_45 { HIDE };
 	
-	/*class CUP_arifle_G36_Base: Rifle_Base_F {};
-	class CUP_arifle_G36A: CUP_arifle_G36_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_G36A_camo: CUP_arifle_G36A { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_G36K: CUP_arifle_G36A { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_G36K_camo: CUP_arifle_G36K { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_G36C: Rifle_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_G36C_camo: CUP_arifle_G36C { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_MG36: CUP_arifle_G36C { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_MG36_camo: CUP_arifle_MG36 { scope = 1; scopeArsenal = 1; scopeCurator = 1; };*/
+	class CUP_arifle_G36_Base: Rifle_Base_F {};
+	class CUP_arifle_G36A: CUP_arifle_G36_Base { HIDE };
+	class CUP_arifle_G36A_camo: CUP_arifle_G36A { HIDE };
+	class CUP_arifle_G36K: CUP_arifle_G36A { HIDE };
+	class CUP_arifle_G36K_camo: CUP_arifle_G36K { HIDE };
+	class CUP_arifle_G36C: Rifle_Base_F { HIDE };
+	class CUP_arifle_G36C_camo: CUP_arifle_G36C { HIDE };
+	class CUP_arifle_MG36: CUP_arifle_G36C { SHOW };
+	class CUP_arifle_MG36_camo: CUP_arifle_MG36 { HIDE };
 	
 	class CUP_arifle_M16_Base: Rifle_Base_F {};
-	class CUP_arifle_M16A4_Base: CUP_arifle_M16_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_M16A4_GL: CUP_arifle_M16_Base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	
-	class CUP_M60E4_base: Rifle_Base_F {};
-	class CUP_lmg_M60E4: CUP_M60E4_base { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	
-	class CUP_srifle_VSSVintorez: Rifle_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_arifle_M16A4_Base: CUP_arifle_M16_Base { HIDE };
+	class CUP_arifle_M16A4_GL: CUP_arifle_M16_Base { HIDE };
 	
 	class Rifle_Long_Base_F: Rifle_Base_F {};
+	class CUP_M60E4_base: Rifle_Long_Base_F {};
+	class CUP_lmg_M60E4: CUP_M60E4_base { HIDE };
 	
-	class CUP_lmg_PKM: Rifle_Long_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_lmg_Pecheneg: CUP_lmg_PKM { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_srifle_VSSVintorez: Rifle_Base_F { HIDE };
 	
-	class CUP_lmg_L110A1: Rifle_Long_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_lmg_PKM: Rifle_Long_Base_F { HIDE };
+	class CUP_lmg_Pecheneg: CUP_lmg_PKM { HIDE };
 	
-	class CUP_lmg_M240: Rifle_Long_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_lmg_L7A2: CUP_lmg_M240 { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_lmg_L110A1: Rifle_Long_Base_F { HIDE };
 	
-	class CUP_srifle_M107_Base: Rifle_Long_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_lmg_M240: Rifle_Long_Base_F { HIDE };
+	class CUP_lmg_L7A2: CUP_lmg_M240 { HIDE };
 	
-	class CUP_sgun_Saiga12K: Rifle_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_srifle_M107_Base: Rifle_Long_Base_F { HIDE };
+	
+	class CUP_sgun_Saiga12K: Rifle_Base_F { HIDE };
 	
 	class Pistol_Base_F;
-	class CUP_hgun_Glock17: Pistol_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_hgun_Glock17: Pistol_Base_F { HIDE };
 	
 	class Launcher_Base_F;
-	class CUP_launch_M136: Launcher_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_launch_M136: Launcher_Base_F { HIDE };
 	
-	class CUP_launch_Mk153Mod0: Launcher_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_launch_Mk153Mod0: Launcher_Base_F { HIDE };
 	
-	class CUP_launch_NLAW: Launcher_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_launch_NLAW: Launcher_Base_F { HIDE };
 	
-	class CUP_arifle_FNFAL: Rifle_Base_F { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_FNFAL5061: CUP_arifle_FNFAL { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_FNFAL5062: CUP_arifle_FNFAL { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_FNFAL_OSW: CUP_arifle_FNFAL { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
-	class CUP_arifle_FNFAL_railed: CUP_arifle_FNFAL { scope = 1; scopeArsenal = 1; scopeCurator = 1; };
+	class CUP_arifle_FNFAL: Rifle_Base_F { HIDE };
+	class CUP_arifle_FNFAL5061: CUP_arifle_FNFAL { HIDE };
+	class CUP_arifle_FNFAL5062: CUP_arifle_FNFAL { HIDE };
+	class CUP_arifle_FNFAL_OSW: CUP_arifle_FNFAL { HIDE };
+	class CUP_arifle_FNFAL_railed: CUP_arifle_FNFAL { HIDE };
+	
+	class CUP_arifle_SCAR_Base: Rifle_Base_F {};
+	class CUP_arifle_SCAR_L_Base: CUP_arifle_SCAR_Base {};
+	class CUP_arifle_Mk16_CQC: CUP_arifle_SCAR_L_Base {};
+	class CUP_arifle_Mk16_CQC_FG: CUP_arifle_Mk16_CQC {};
+	class CUP_arifle_Mk16_CQC_SFG: CUP_arifle_Mk16_CQC {};
+	class CUP_arifle_Mk16_CQC_EGLM: CUP_arifle_Mk16_CQC {};
+	class CUP_arifle_Mk16_STD: CUP_arifle_SCAR_L_Base {};
+	class CUP_arifle_Mk16_STD_FG: CUP_arifle_Mk16_STD {};
+	class CUP_arifle_Mk16_STD_SFG: CUP_arifle_Mk16_STD {};
+	class CUP_arifle_Mk16_STD_EGLM: CUP_arifle_Mk16_STD {};
+	class CUP_arifle_Mk16_SV: CUP_arifle_SCAR_L_Base {};
+	class CUP_arifle_Mk17_Base: CUP_arifle_SCAR_Base {};
+	class CUP_arifle_Mk17_CQC: CUP_arifle_Mk17_Base {};
+	class CUP_arifle_Mk17_CQC_FG: CUP_arifle_Mk17_CQC {};
+	class CUP_arifle_Mk17_CQC_SFG: CUP_arifle_Mk17_CQC_FG {};
+	class CUP_arifle_Mk17_CQC_EGLM: CUP_arifle_Mk17_CQC {};
+	class CUP_arifle_Mk17_STD: CUP_arifle_Mk17_Base {};
+	class CUP_arifle_Mk17_STD_FG: CUP_arifle_Mk17_STD {};
+	class CUP_arifle_Mk17_STD_SFG: CUP_arifle_Mk17_STD_FG {};
+	class CUP_arifle_Mk17_STD_EGLM: CUP_arifle_Mk17_STD {};
+	class CUP_arifle_Mk20: CUP_arifle_Mk17_Base {};
+	class CUP_arifle_Mk16_CQC_woodland: CUP_arifle_Mk16_CQC { HIDE };
+	class CUP_arifle_Mk16_CQC_FG_woodland: CUP_arifle_Mk16_CQC_FG { HIDE };
+	class CUP_arifle_Mk16_CQC_SFG_woodland: CUP_arifle_Mk16_CQC_SFG { HIDE };
+	class CUP_arifle_Mk16_CQC_EGLM_woodland: CUP_arifle_Mk16_CQC_EGLM { HIDE };
+	class CUP_arifle_Mk16_STD_woodland: CUP_arifle_Mk16_STD { HIDE };
+	class CUP_arifle_Mk16_STD_FG_woodland: CUP_arifle_Mk16_STD_FG { HIDE };
+	class CUP_arifle_Mk16_STD_SFG_woodland: CUP_arifle_Mk16_STD_SFG { HIDE };
+	class CUP_arifle_Mk16_STD_EGLM_woodland: CUP_arifle_Mk16_STD_EGLM { HIDE };
+	class CUP_arifle_Mk16_SV_woodland: CUP_arifle_Mk16_SV { HIDE };
+	class CUP_arifle_Mk17_CQC_woodland: CUP_arifle_Mk17_CQC { HIDE };
+	class CUP_arifle_Mk17_CQC_FG_woodland: CUP_arifle_Mk17_CQC_FG { HIDE };
+	class CUP_arifle_Mk17_CQC_SFG_woodland: CUP_arifle_Mk17_CQC_SFG { HIDE };
+	class CUP_arifle_Mk17_CQC_EGLM_woodland: CUP_arifle_Mk17_CQC_EGLM { HIDE };
+	class CUP_arifle_Mk17_STD_woodland: CUP_arifle_Mk17_STD { HIDE };
+	class CUP_arifle_Mk17_STD_FG_woodland: CUP_arifle_Mk17_STD_FG { HIDE };
+	class CUP_arifle_Mk17_STD_SFG_woodland: CUP_arifle_Mk17_STD_SFG { HIDE };
+	class CUP_arifle_Mk17_STD_EGLM_woodland: CUP_arifle_Mk17_STD_EGLM { HIDE };
+	class CUP_arifle_Mk16_CQC_black: CUP_arifle_Mk16_CQC { HIDE };
+	class CUP_arifle_Mk16_CQC_FG_black: CUP_arifle_Mk16_CQC_FG { HIDE };
+	class CUP_arifle_Mk16_CQC_SFG_black: CUP_arifle_Mk16_CQC_SFG { HIDE };
+	class CUP_arifle_Mk16_CQC_EGLM_black: CUP_arifle_Mk16_CQC_EGLM { HIDE };
+	class CUP_arifle_Mk16_STD_black: CUP_arifle_Mk16_STD { HIDE };
+	class CUP_arifle_Mk16_STD_FG_black: CUP_arifle_Mk16_STD_FG { HIDE };
+	class CUP_arifle_Mk16_STD_SFG_black: CUP_arifle_Mk16_STD_SFG { HIDE };
+	class CUP_arifle_Mk16_STD_EGLM_black: CUP_arifle_Mk16_STD_EGLM { HIDE };
+	class CUP_arifle_Mk16_SV_black: CUP_arifle_Mk16_SV { HIDE };
+	class CUP_arifle_Mk17_CQC_Black: CUP_arifle_Mk17_CQC { HIDE };
+	class CUP_arifle_Mk17_CQC_FG_black: CUP_arifle_Mk17_CQC_FG { HIDE };
+	class CUP_arifle_Mk17_CQC_SFG_black: CUP_arifle_Mk17_CQC_SFG { HIDE };
+	class CUP_arifle_Mk17_CQC_EGLM_black: CUP_arifle_Mk17_CQC_EGLM { HIDE };
+	class CUP_arifle_Mk17_STD_black: CUP_arifle_Mk17_STD { HIDE };
+	class CUP_arifle_Mk17_STD_FG_black: CUP_arifle_Mk17_STD_FG { HIDE };
+	class CUP_arifle_Mk17_STD_SFG_black: CUP_arifle_Mk17_STD_SFG { HIDE };
+	class CUP_arifle_Mk17_STD_EGLM_black: CUP_arifle_Mk17_STD_EGLM { HIDE };
 };
