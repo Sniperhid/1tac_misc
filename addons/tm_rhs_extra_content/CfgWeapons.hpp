1d08b66505c3bd27466ae08cc3f85099adc6e96a
Bear <nipronz@gmail.com>
2017-10-01 20:26:22 +0200

    finish

--- addons/tm_rhs_extra_content/CfgWeapons.hpp
+++ addons/tm_rhs_extra_content/CfgWeapons.hpp
@@ -10,50 +10,64 @@ class CfgWeapons {
 	class rhsusf_hgu56p;
 	class rhsusf_hgu56p_visor: rhsusf_hgu56p {};
 	class tm_hgu56p_visor_cav: rhsusf_hgu56p_visor {
+		author = "Red Hammer Studios";
 		displayName = "HGU-56/P (Olive/Visor/Cav)";
 		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_cav_co.paa"};
 	};
 	class rhsusf_hgu56p_visor_mask: rhsusf_hgu56p {};
 	class tm_hgu56p_visor_mask_cav: rhsusf_hgu56p_visor_mask {
+		author = "Red Hammer Studios";
 		displayName = "HGU-56/P (Olive/Visor/Mask/Cav)";
 		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_cav_co.paa"};
 	};
 	class tm_hgu56p_visor_mask_orange_sharpie: rhsusf_hgu56p_visor_mask {
+		author = "Red Hammer Studios";
 		displayName = "HGU-56/P (Orange/Visor/Mask/Sharpie)";
 		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_qikaz_co.paa"};
 		picture = "\rhsusf\addons\rhsusf_infantry2\ui\rhs_hgu56_visor_mask_olv_smiley_ca.paa";
 	};
 	class rhsusf_hgu56p_black: rhsusf_hgu56p {};
-	class rhsusf_hgu56p_visor_mask_Empire_black: rhsusf_hgu56p_black {};
-	class tm_hgu56p_visor_mask_1tac_black: rhsusf_hgu56p_visor_mask_Empire_black {
+	class rhsusf_hgu56p_visor_black: rhsusf_hgu56p_black {};
+	class tm_hgu56p_visor_1tac_black: rhsusf_hgu56p_visor_black {
 		author = "Red Hammer Studios, 1TAC Misc";
+		displayName = "HGU-56/P (Black/Visor/1TAC)";
+		hiddenSelectionsTextures[] = {
+			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_blk_co.paa",
+			"\x\tac1\addons\tm_rhs_extra_content\data\rhs_hgu56_patches_1tac_co.paa"
+		};
+	};
+	class tm_hgu56p_visor_rebel: rhsusf_hgu56p_visor_black {
+		author = "Red Hammer Studios";
+		displayName = "HGU-56/P (Rebel/Visor)";
+		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_x_co.paa"};
+	};
+	class tm_hgu56p_visor_nukacola: rhsusf_hgu56p_visor_black {
+		author = "Red Hammer Studios";
+		displayName = "HGU-56/P (Nuka Cola/Visor)";
+		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_cola_co.paa"};
+	};
+	class rhsusf_hgu56p_visor_mask_black: rhsusf_hgu56p_black {};
+	class tm_hgu56p_visor_mask_1tac_black: rhsusf_hgu56p_visor_mask_black {
+		author = "Red Hammer Studios";
 		displayName = "HGU-56/P (Black/Visor/Mask/1TAC)";
 		hiddenSelectionsTextures[] = {
 			"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_blk_co.paa",
 			"\x\tac1\addons\tm_rhs_extra_content\data\rhs_hgu56_patches_1tac_co.paa"
 		};
 	};
-	class rhsusf_hgu56p_visor_black: rhsusf_hgu56p_black {};
-	class tm_hgu56p_visor_rebel: rhsusf_hgu56p_visor_black {
-		displayName = "HGU-56/P (Rebel/Visor)";
-		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_x_co.paa"};
-	};
-	class tm_hgu56p_visor_nukacola: rhsusf_hgu56p_visor_black {
-		displayName = "HGU-56/P (Nuka Cola/Visor)";
-		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_cola_co.paa"};
-	};
-	class rhsusf_hgu56p_visor_mask_black: rhsusf_hgu56p_black {};
 	class tm_hgu56p_visor_mask_rebel: rhsusf_hgu56p_visor_mask_black {
+		author = "Red Hammer Studios";
 		displayName = "HGU-56/P (Rebel/Visor/Mask)";
 		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_x_co.paa"};
 	};
 	class tm_hgu56p_visor_mask_nukacola: rhsusf_hgu56p_visor_mask_black {
+		author = "Red Hammer Studios";
 		displayName = "HGU-56/P (Nuka Cola/Visor/Mask)";
 		hiddenSelectionsTextures[] = {"\rhsusf\addons\rhsusf_infantry2\gear\head\hgu56\Data\rhs_hgu56_cola_co.paa"};
 	};
 	
 	class rhsusf_iotv_ocp_base;
 	class rhsusf_plateframe_sapi: rhsusf_iotv_ocp_base {
-		scope = 2;
+		scope = 1;
 	};
 };
