a74b941896db0c6d7bf1b43631731075c5de60e6
Bear <nipronz@gmail.com>
2017-10-03 17:37:26 +0200

    less vehicle actions

---
+++ addons/tm_rhs_vehicle_actions/config.cpp
@@ -0,0 +1,22 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+	class ADDON {
+		name = "Tweak availability of RHS vehicle user actions such as opening rear M113 door";
+		author = "Bear";
+		url = "http://www.teamonetactical.com";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {
+			"RHS_US_A2_AirImport",
+			"RHS_US_A2Port_Armor",
+			"rhsusf_c_Caiman",
+			"rhsusf_c_m113",
+			"rhsusf_c_RG33L"
+		};
+		requiredVersion = REQUIRED_VERSION;
+		VERSION_CONFIG;
+	};
+};
+
+#include "CfgVehicles.hpp"
