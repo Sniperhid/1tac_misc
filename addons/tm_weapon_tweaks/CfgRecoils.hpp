277dc1111e8c03871eae7ab52a8e14e2f9eab243
Bear <beaar@users.noreply.github.com>
2017-12-30 19:30:29 +0100

    recoil and dispersion of common weapons (#101)

---
+++ addons/tm_weapon_tweaks/CfgRecoils.hpp
@@ -0,0 +1,19 @@
+class CfgRecoils {
+    class recoil_mk200;
+    class tm_recoil_m249: recoil_mk200 {
+        muzzleOuter[] = {"0.4*0.3", "0.85*1", "0.4*1", "0.2*1"};
+        kickBack[] = {"0.045*1.4", "0.07*1.4"};
+        temporary = "0.012";
+    };
+    
+    class recoil_zafir;
+    class tm_recoil_m240: recoil_zafir {
+        kickBack[] = {"0.02*1.3","0.08*1.4"};
+        muzzleOuter[] = {"0.5*0.2","1*1","0.7*1","0.3*1"};
+    };
+    
+    class recoil_mxc;
+    class tm_recoil_battlerifle: recoil_mxc {
+        kickBack[] = {"0.03*1.4","0.08*1.4"};
+    };
+};
