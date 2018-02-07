415a5cfecea657e7fb1a0a7a63e9e3b963383ac6
Bear <beaar@users.noreply.github.com>
2018-02-07 22:50:16 +0100

    tweak faction names (#116)
    add [rhs] suffix

---
+++ addons/tm_faction_names/CfgGroups.hpp
@@ -0,0 +1,60 @@
+class CfgGroups {
+    class East {
+        class rhs_faction_msv {
+            name = "Russia (MSV) [RHS]";
+        };
+        class rhs_faction_tv {
+            name = "Russia (TV) [RHS]";
+        };
+        class rhs_faction_vdv {
+            name = "Russia (VDV) [RHS]";
+        };
+        class rhs_faction_vmf {
+            name = "Russia (VMF) [RHS]";
+        };
+        
+        class rhsgref_faction_chdkz {
+            name = "ChDKZ Insurgents [RHS]";
+        };
+    };
+    
+    class West {
+        class rhs_faction_usarmy_d {
+            name = "USA (Army - D) [RHS]";
+        };
+        class rhs_faction_usarmy_wd {
+            name = "USA (Army - W) [RHS]";
+        };
+        class rhs_faction_usmc_d {
+            name = "USA (USMC - D) [RHS]";
+        };
+        class rhs_faction_usmc_wd {
+            name = "USA (USMC - W) [RHS]";
+        };
+        class rhs_faction_socom_marsoc {
+            name = "USA (SOCOM) [RHS]";
+        };
+        
+        class rhsgref_faction_cdf_b_ground {
+            name = "Chernarus (Ground Forces) [RHS]";
+        };
+        class rhsgref_faction_hidf {
+            name = "Horizon Islands Defence Force [RHS]";
+        };
+    };
+    
+    class Indep {
+        class rhsgref_faction_cdf_ground {
+            name = "Chernarus (Ground Forces) [RHS]";
+        };
+        class rhsgref_faction_cdf_ng {
+            name = "Chernarus (National Guard) [RHS]";
+        };
+        class rhsgref_faction_nationalist {
+            name = "Nationalist Militia [RHS]";
+        };
+        class rhsgref_faction_chdkz_g {
+            name = "ChDKZ Insurgents [RHS]";
+        };
+    };
+};
