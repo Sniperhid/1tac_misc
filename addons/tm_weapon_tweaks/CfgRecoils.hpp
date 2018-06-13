f0ae7d8caec998a9aa75fcc76f292ada877fd414
Bear <nipronz@gmail.com>
2018-06-13 13:23:00 +0200

    Upset people

--- addons/tm_weapon_tweaks/CfgRecoils.hpp
+++ addons/tm_weapon_tweaks/CfgRecoils.hpp
@@ -1,14 +1,14 @@
 class CfgRecoils {
     class recoil_mk200;
     class tm_recoil_m249: recoil_mk200 {
-        muzzleOuter[] = {"0.4*0.3", "0.85*1", "0.4*1", "0.2*1"};
-        kickBack[] = {"0.045*1.4", "0.07*1.4"};
+        muzzleOuter[] = {"0.3", "0.7", "0.4*1", "0.4*1"};
+        kickBack[] = {"0.04", "0.06"};
         temporary = "0.012";
     };
     
     class recoil_zafir;
     class tm_recoil_m240: recoil_zafir {
-        kickBack[] = {"0.02*1.3","0.06*1.4"};
+        kickBack[] = {"0.02","0.06*1.2"};
         muzzleOuter[] = {"0.5*0.2","0.8*1","0.6*1","0.25*1"};
     };
     
