fa2750ed3bb311c596896e145c11da90ae828cb2
Bear <nipronz@gmail.com>
2018-06-26 10:35:18 +0200

    cantdoanythingright

--- addons/tm_dummy_addons/ace_tracers/config.cpp
+++ addons/tm_dummy_addons/ace_tracers/config.cpp
@@ -1,14 +1,15 @@
-#include "\x\tac1\addons\tm_dummy_addons\script_component.hpp"
+#include "script_component.hpp"
 
 class CfgPatches {
-    class ace_tracers {
-        name = "1TAC_MISC Dummy: ACE Tracers";
-        author = "1TAC_MISC";
-        url = "http://www.teamonetactical.com";
+    class ADDON {
+        name = COMPONENT_NAME;
         units[] = {};
         weapons[] = {};
-        requiredAddons[] = { QUOTE(ADDON) };
         requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {};
+        author = ECSTRING(common,ACETeam);
+        authors[] = {"ACE2 Team"};
+        url = ECSTRING(main,URL);
         VERSION_CONFIG;
     };
 };
