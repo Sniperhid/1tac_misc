234dc66fe03fa8c3b93fabbbf50ba5046d150463
Bear <nipronz@gmail.com>
2018-07-30 23:34:32 +0200

    Dummy addon

---
+++ addons/tm_dummy_addons/tf47_launchers/config.cpp
@@ -0,0 +1,44 @@
+#include "\x\tac1\addons\tm_dummy_addons\script_component.hpp"
+
+class CfgPatches {
+    class tf47_launchers {
+        name = "1TAC_MISC Dummy: TF47 Launchers";
+        author = "1TAC_MISC";
+        url = "http://www.teamonetactical.com";
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = { QUOTE(ADDON) };
+        requiredVersion = REQUIRED_VERSION;
+        VERSION_CONFIG;
+    };
+    class tf47_at4 {
+        name = "1TAC_MISC Dummy: TF47 Launchers";
+        author = "1TAC_MISC";
+        url = "http://www.teamonetactical.com";
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = { QUOTE(ADDON) };
+        requiredVersion = REQUIRED_VERSION;
+        VERSION_CONFIG;
+    };
+    class tf47_m3maaws {
+        name = "1TAC_MISC Dummy: TF47 Launchers";
+        author = "1TAC_MISC";
+        url = "http://www.teamonetactical.com";
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = { QUOTE(ADDON) };
+        requiredVersion = REQUIRED_VERSION;
+        VERSION_CONFIG;
+    };
+    class tf47_smaw {
+        name = "1TAC_MISC Dummy: TF47 Launchers";
+        author = "1TAC_MISC";
+        url = "http://www.teamonetactical.com";
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = { QUOTE(ADDON) };
+        requiredVersion = REQUIRED_VERSION;
+        VERSION_CONFIG;
+    };
+};
