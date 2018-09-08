79024ca044b87fff3b03472abeda6090a045b1f1
Bear <beaar@users.noreply.github.com>
2018-09-08 17:07:32 +0200

    Disable RHS lead (#184)
    * Disable RHS lead
    
    * Change to PostInit

---
+++ addons/tm_disable_rhs_fcs_lead/config.cpp
@@ -0,0 +1,20 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = "Disable RHS FCS lead";
+        author = "Bear";
+        url = "http://www.teamonetactical.com";
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = {"rhs_main", "rhsusf_main"};
+        requiredVersion = REQUIRED_VERSION;
+        VERSION_CONFIG;
+    };
+};
+
+class Extended_PostInit_EventHandlers {
+    class tm_disable_rhs_fcs_lead {
+        init = "profileNamespace setVariable ['rhs_fcsTargetLead', 0]; saveProfileNamespace;";
+    };
+};
