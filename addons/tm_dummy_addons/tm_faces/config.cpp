d2e37e8dc2026d2add7118bffc238d59efa9ce6a
Bear <nipronz@gmail.com>
2018-10-16 13:13:30 +0200

    remove tm_faces

---
+++ addons/tm_dummy_addons/tm_faces/config.cpp
@@ -0,0 +1,14 @@
+#include "\x\tac1\addons\tm_dummy_addons\script_component.hpp"
+
+class CfgPatches {
+    class tm_faces {
+        name = "1TAC_MISC Dummy: tm_faces";
+        author = "1TAC_MISC";
+        url = "http://www.teamonetactical.com";
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = { QUOTE(ADDON) };
+        requiredVersion = REQUIRED_VERSION;
+        VERSION_CONFIG;
+    };
+};
