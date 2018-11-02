c364824f6ec2fb3e2dcb6e1ddf2b09008f026d6b
Bear <beaar@users.noreply.github.com>
2018-11-02 11:44:01 +0100

    Overwrite terrain dust (#215)

---
+++ addons/tm_terrain_dust/config.cpp
@@ -0,0 +1,24 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = "Force terrain dust effects";
+        author = "Bear";
+        url = "http://www.teamonetactical.com";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "tac1_tm_main",
+            "CUP_Worlds_DustEffects",
+            "A3_Map_IslaDuala3",
+            "tem_anizay",
+            "Hellanmaa",
+            "pulau",
+            "Ruha"
+        };
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
