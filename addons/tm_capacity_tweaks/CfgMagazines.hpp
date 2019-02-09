818931aa15083aaad91ed3ee990e060d9280fde5
Bear <beaar@users.noreply.github.com>
2019-02-09 16:05:29 +0100

    M60 ammo weight reduction (#248)

--- addons/tm_capacity_tweaks/CfgMagazines.hpp
+++ addons/tm_capacity_tweaks/CfgMagazines.hpp
@@ -1,8 +1,11 @@
-class CfgMagazines
-{
-	class CA_LauncherMagazine;
-	class rhs_fgm148_magazine_AT: CA_LauncherMagazine 
-	{
-		mass = 200; // 286.88
-	};
+class CfgMagazines {
+    class CA_LauncherMagazine;
+    class rhs_fgm148_magazine_AT: CA_LauncherMagazine {
+        mass = 200; // 286.88
+    };
+    
+    class 30Rnd_556x45_Stanag;
+    class hlc_100Rnd_762x51_B_M60E4: 30Rnd_556x45_Stanag {
+        mass = 36;
+    };
 };
