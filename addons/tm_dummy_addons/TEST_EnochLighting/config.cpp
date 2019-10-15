e9efc6903786f7471d812eff33aef475aa503def
Bear <nipronz@gmail.com>
2019-10-15 12:02:06 +0200

    TEST_EnochLighting dummy

---
+++ addons/tm_dummy_addons/TEST_EnochLighting/config.cpp
@@ -0,0 +1,14 @@
+#include "\x\tac1\addons\tm_dummy_addons\script_component.hpp"
+
+class CfgPatches {
+    class TEST_EnochLighting {
+        name = "1TAC_MISC Dummy: TEST_EnochLighting";
+        author = "1TAC_MISC";
+        url = "http://www.teamonetactical.com";
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = { QUOTE(ADDON) };
+        requiredVersion = REQUIRED_VERSION;
+        VERSION_CONFIG;
+    };
+};
