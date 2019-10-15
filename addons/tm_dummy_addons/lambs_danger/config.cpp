b1276418a7c3f9d82ac07d4956252545aa319e7c
Bear <beaar@users.noreply.github.com>
2019-10-15 11:40:19 +0200

    lambs danger dummy (#286)

---
+++ addons/tm_dummy_addons/lambs_danger/config.cpp
@@ -0,0 +1,14 @@
+#include "\x\tac1\addons\tm_dummy_addons\script_component.hpp"
+
+class CfgPatches {
+    class lambs_danger {
+        name = "1TAC_MISC Dummy: lambs_danger";
+        author = "1TAC_MISC";
+        url = "http://www.teamonetactical.com";
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = { QUOTE(ADDON) };
+        requiredVersion = REQUIRED_VERSION;
+        VERSION_CONFIG;
+    };
+};
