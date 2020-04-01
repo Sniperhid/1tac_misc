c9db5b484c7d9ef689a9b1968925f51a666b819e
Bear <nipronz@gmail.com>
2020-04-01 17:25:57 +0200

    invisible backpack

---
+++ addons/tm_dummy_addons/Battle_Belt/config.cpp
@@ -0,0 +1,14 @@
+#include "\x\tac1\addons\tm_dummy_addons\script_component.hpp"
+
+class CfgPatches {
+    class Battle_Belt {
+        name = "1TAC_MISC Dummy: Battle_Belt";
+        author = "1TAC_MISC";
+        url = "http://www.teamonetactical.com";
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = { QUOTE(ADDON) };
+        requiredVersion = REQUIRED_VERSION;
+        VERSION_CONFIG;
+    };
+};
