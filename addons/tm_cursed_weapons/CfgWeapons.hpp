c6409220768c576e693056c16f67efb87b7ad6fd
Bear <beaar@users.noreply.github.com>
2020-05-18 01:24:22 +0200

    Norinco M305A (#325)

---
+++ addons/tm_cursed_weapons/CfgWeapons.hpp
@@ -0,0 +1,10 @@
+class CfgWeapons {
+    class rhs_weap_m14;
+    class GVAR(m305a): rhs_weap_m14 {
+        displayName = "M305A";
+        baseWeapon = QGVAR(m305a);
+        author = "1Tac Misc";
+        magazines[] = {};
+        magazineWell[] = {"AK_762x39","CBA_762x39_AK","CBA_762x39_RPK"};
+    };
+};
