674bf03496b59d7000fe29f1e81dd9e5b5d77989
Bear <beaar@users.noreply.github.com>
2020-07-21 14:22:45 +0200

    New members (#340)

--- addons/tm_admin/config.cpp
+++ addons/tm_admin/config.cpp
@@ -2,34 +2,13 @@
 
 class CfgPatches {
     class ADDON {
-        name = "1TAC Admin whitelist";
+        name = "";
         author = "Bear";
         url = "http://www.teamonetactical.com";
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"tac1_tm_main", "tmf_adminmenu"};
+        requiredAddons[] = {"tac1_tm_main"};
         VERSION_CONFIG;
-    };
-};
-
-class tmf_adminmenu_authorized_players {
-    class Bear {
-        uid = "76561198014669991";
-    };
-    class Nick {
-        uid = "76561198031183429";
-    };
-    class Sam {
-        uid = "76561198010479904";
-    };
-    class Snake {
-        uid = "76561198052428514";
-    };
-    class Snippers {
-        uid = "76561197974275147";
-    };
-    class Titan {
-        uid = "76561198002666327";
     };
 };
