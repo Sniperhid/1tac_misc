0938da9b929e150e875edb44f8d18771d51fbd07
Kasmeister <30311423+Kasmeister@users.noreply.github.com>
2021-10-02 20:03:28 +0200

    Update CfgLoadouts.hpp

--- addons/tm_tmf_loadouts/CfgLoadouts.hpp
+++ addons/tm_tmf_loadouts/CfgLoadouts.hpp
@@ -1223,12 +1223,6 @@ class CfgLoadouts
 	category = "Other";
 	#include "loadouts\taliban_regular_2021.hpp"
     };
-    class TALIBAN_ELITE_2021
-    {
-	displayName = "Taliban Elite Fighters (2021-, AKMs&M4s)";
-	category = "Other";
-	#include "loadouts\taliban_elite_2021.hpp"
-    };
     
     
     class LIVONIAN_DEFENCE_FORCE
